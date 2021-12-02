f8711b5bf51ec7ce4b6d44ee29311aafe2d687ed
Fernando_Heisser <Fernando_Heisser@dell.com>
2021-12-01 22:53:09 -0300

    starting to create pages

---
+++ web/src/pages/Login/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Login = () =>{
+    return(
+        <div id="page-home">
+            <div className="content">
+                <main>
+                    <h1>Seu marketplace de coleta de resíduos.</h1>
+                    <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+                </main>
+            </div>
+        </div>
+    );
+}
+export default Login;
