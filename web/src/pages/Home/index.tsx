62f261c46e036c0a7f1d03434db0f2b68b1a11bb
Fernando_Heisser <Fernando_Heisser@dell.com>
2021-12-04 21:56:55 -0300

    creating home page

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,12 +1,209 @@
 import React from 'react';
+import './home.css'
 
 const Login = () => {
     return (
         <div id="page-home">
             <div className="content">
-                <main>
-                    <h1>Seu marketplace de coleta de resíduos.</h1>
-                    <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+                <header className='home-header'>
+                    <div className='header-left'>
+                        <h1 className='home-title'>Bah Respostas</h1>
+                    </div>
+                    <div className='header-middle'>
+                        <input className='home-header-input' type="text" placeholder='Procurar no Bah Respostas' />
+                    </div>
+                    <div className='header-right'>
+                        <p className='header-message'>Olá, Fernando</p>
+                        <button className='header-button'>Sair</button>
+                    </div>
+                </header>
+                <main className='home-main'>
+                    <div className='home-main-left'>
+                        <div className='home-left-menu'>
+                            <div className='home-left-menu-header'>
+                                <p>Respostas recentes</p>
+                            </div>
+                            <div className='home-left-menu-content'>
+                                <ul className="home-left-menu-list">
+                                    <li>
+                                        <p className="home-left-menu-item-content">Cara, pra ser bem sincero, acho que tu deveria desistir...</p>
+                                        <p className="home-left-menu-item-user">RaposaViolenta34325</p>
+                                    </li>
+                                    <li>
+                                        <p className="home-left-menu-item-content">Coloca no lugar no int um long e não esquece do L no final do valor.</p>
+                                        <p className="home-left-menu-item-user">tio_do_pastelltz</p>
+                                    </li>
+                                    <li>
+                                        <p className="home-left-menu-item-content">Com maionese e ketchup com certeza.</p>
+                                        <p className="home-left-menu-item-user">dragonballlgtt</p>
+                                    </li>
+                                    <li>
+                                        <p className="home-left-menu-item-content">Será que tu não tinha outra ideia melhor não?</p>
+                                        <p className="home-left-menu-item-user">xxxCruzxxx</p>
+                                    </li>
+                                    <li>
+                                        <p className="home-left-menu-item-content">Amarelo com azul dá verde.</p>
+                                        <p className="home-left-menu-item-user">falcatruax9</p>
+                                    </li>
+                                    <li>
+                                        <p className="home-left-menu-item-content">Sim.</p>
+                                        <p className="home-left-menu-item-user">Vitoria56325</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='home-main-middle'>
+                        <ul className="home-main-middle-list">
+                            <li>
+                                <div className='question-card'>
+                                    <div className='question-card-header'>
+                                        <h1 className='question-card-title'> Com quantos paus se faz uma canoa olimpica, no caso se for feito com madeira, hein?</h1>
+                                    </div>
+                                    <div className='question-card-content'>
+                                        <p className='question-card-body'>Do nada eu tava pensando, como que se faz as canoas olimpicas o maluco brasileiro manda muito bem na canoagem mas quem faz essa canoa faz com o que?</p>
+                                    </div>
+                                    <div className='question-card-footer'>
+                                        <div className='question-card-answers'>
+                                            <button className='question-card-answer-button'>Responder</button>
+                                            <span className='question-card-number-answers'>6</span>
+                                            <p className='question-card-answers-text'>respostas</p>
+                                        </div>
+                                        <div className='question-card-user-information'>
+                                            <p className='card-user-nickname'>JailsonMendesLaranja123</p>
+                                        </div>
+                                    </div>
+                                    <div className='question-card-answer'>
+                                        <p className='question-card-answer-body'>Cara eu acho que mais ou menos 34 paus tu consegue uma canoa bem massa.</p>
+                                        <p className='question-card-answer-user'>PaulinhoGOGO7367</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='question-card'>
+                                    <div className='question-card-header'>
+                                        <h1 className='question-card-title'> Com quantos paus se faz uma canoa olimpica, no caso se for feito com madeira, hein?</h1>
+                                    </div>
+                                    <div className='question-card-content'>
+                                        <p className='question-card-body'>Do nada eu tava pensando, como que se faz as canoas olimpicas o maluco brasileiro manda muito bem na canoagem mas quem faz essa canoa faz com o que?</p>
+                                    </div>
+                                    <div className='question-card-footer'>
+                                        <div className='question-card-answers'>
+                                            <button className='question-card-answer-button'>Responder</button>
+                                            <span className='question-card-number-answers'>6</span>
+                                            <p className='question-card-answers-text'>respostas</p>
+                                        </div>
+                                        <div className='question-card-user-information'>
+                                            <p className='card-user-nickname'>JailsonMendesLaranja123</p>
+                                        </div>
+                                    </div>
+                                    <div className='question-card-answer'>
+                                        <p className='question-card-answer-body'>Cara eu acho que mais ou menos 34 paus tu consegue uma canoa bem massa.</p>
+                                        <p className='question-card-answer-user'>PaulinhoGOGO7367</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='question-card'>
+                                    <div className='question-card-header'>
+                                        <h1 className='question-card-title'> Com quantos paus se faz uma canoa olimpica, no caso se for feito com madeira, hein?</h1>
+                                    </div>
+                                    <div className='question-card-content'>
+                                        <p className='question-card-body'>Do nada eu tava pensando, como que se faz as canoas olimpicas o maluco brasileiro manda muito bem na canoagem mas quem faz essa canoa faz com o que?</p>
+                                    </div>
+                                    <div className='question-card-footer'>
+                                        <div className='question-card-answers'>
+                                            <button className='question-card-answer-button'>Responder</button>
+                                            <span className='question-card-number-answers'>6</span>
+                                            <p className='question-card-answers-text'>respostas</p>
+                                        </div>
+                                        <div className='question-card-user-information'>
+                                            <p className='card-user-nickname'>JailsonMendesLaranja123</p>
+                                        </div>
+                                    </div>
+                                    <div className='question-card-answer'>
+                                        <p className='question-card-answer-body'>Cara eu acho que mais ou menos 34 paus tu consegue uma canoa bem massa.</p>
+                                        <p className='question-card-answer-user'>PaulinhoGOGO7367</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='question-card'>
+                                    <div className='question-card-header'>
+                                        <h1 className='question-card-title'> Com quantos paus se faz uma canoa olimpica, no caso se for feito com madeira, hein?</h1>
+                                    </div>
+                                    <div className='question-card-content'>
+                                        <p className='question-card-body'>Do nada eu tava pensando, como que se faz as canoas olimpicas o maluco brasileiro manda muito bem na canoagem mas quem faz essa canoa faz com o que?</p>
+                                    </div>
+                                    <div className='question-card-footer'>
+                                        <div className='question-card-answers'>
+                                            <button className='question-card-answer-button'>Responder</button>
+                                            <span className='question-card-number-answers'>6</span>
+                                            <p className='question-card-answers-text'>respostas</p>
+                                        </div>
+                                        <div className='question-card-user-information'>
+                                            <p className='card-user-nickname'>JailsonMendesLaranja123</p>
+                                        </div>
+                                    </div>
+                                    <div className='question-card-answer'>
+                                        <p className='question-card-answer-body'>Cara eu acho que mais ou menos 34 paus tu consegue uma canoa bem massa.</p>
+                                        <p className='question-card-answer-user'>PaulinhoGOGO7367</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='question-card'>
+                                    <div className='question-card-header'>
+                                        <h1 className='question-card-title'> Com quantos paus se faz uma canoa olimpica, no caso se for feito com madeira, hein?</h1>
+                                    </div>
+                                    <div className='question-card-content'>
+                                        <p className='question-card-body'>Do nada eu tava pensando, como que se faz as canoas olimpicas o maluco brasileiro manda muito bem na canoagem mas quem faz essa canoa faz com o que?</p>
+                                    </div>
+                                    <div className='question-card-footer'>
+                                        <div className='question-card-answers'>
+                                            <button className='question-card-answer-button'>Responder</button>
+                                            <span className='question-card-number-answers'>6</span>
+                                            <p className='question-card-answers-text'>respostas</p>
+                                        </div>
+                                        <div className='question-card-user-information'>
+                                            <p className='card-user-nickname'>JailsonMendesLaranja123</p>
+                                        </div>
+                                    </div>
+                                    <div className='question-card-answer'>
+                                        <p className='question-card-answer-body'>Cara eu acho que mais ou menos 34 paus tu consegue uma canoa bem massa.</p>
+                                        <p className='question-card-answer-user'>PaulinhoGOGO7367</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='question-card'>
+                                    <div className='question-card-header'>
+                                        <h1 className='question-card-title'> Com quantos paus se faz uma canoa olimpica, no caso se for feito com madeira, hein?</h1>
+                                    </div>
+                                    <div className='question-card-content'>
+                                        <p className='question-card-body'>Do nada eu tava pensando, como que se faz as canoas olimpicas o maluco brasileiro manda muito bem na canoagem mas quem faz essa canoa faz com o que?</p>
+                                    </div>
+                                    <div className='question-card-footer'>
+                                        <div className='question-card-answers'>
+                                            <button className='question-card-answer-button'>Responder</button>
+                                            <span className='question-card-number-answers'>6</span>
+                                            <p className='question-card-answers-text'>respostas</p>
+                                        </div>
+                                        <div className='question-card-user-information'>
+                                            <p className='card-user-nickname'>JailsonMendesLaranja123</p>
+                                        </div>
+                                    </div>
+                                    <div className='question-card-answer'>
+                                        <p className='question-card-answer-body'>Cara eu acho que mais ou menos 34 paus tu consegue uma canoa bem massa.</p>
+                                        <p className='question-card-answer-user'>PaulinhoGOGO7367</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='home-main-right'>
+                        <button className='home-right-button'>+</button>
+                    </div>
                 </main>
             </div>
         </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Route, BrowserRouter} from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Home from './pages/Home';
 
-const Routes = () =>{
+const Routes = () => {
     return (
         <BrowserRouter >
-            <Route component={Login} path="/" exact />
-            <Route component={Home} path="/home" />
+            <Route component={Login} path="/home" exact />
+            <Route component={Home} path="/" />
         </BrowserRouter>
     );
 }
